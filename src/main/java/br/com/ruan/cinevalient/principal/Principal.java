70817c821a839471ed9ef6082ad15add09cc7769
Ruan Silva <ruan.silva2002@gmail.com>
2025-10-03 00:17:43 -0300

    Update na class Principal, a qual foi colocado os métodos de pesquisa por episódios e a implementação do método lambda

--- src/main/java/br/com/ruan/cinevalient/principal/Principal.java
+++ src/main/java/br/com/ruan/cinevalient/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.ruan.cinevalient.principal;
 
+import br.com.ruan.cinevalient.model.DadosEpisodios;
 import br.com.ruan.cinevalient.model.DadosSerie;
 import br.com.ruan.cinevalient.model.DadosTemporada;
 import br.com.ruan.cinevalient.service.ConsumoAPI;
@@ -36,5 +37,14 @@ public class Principal {
         }
 
         temporadas.forEach(System.out::println);
+
+//        for (int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DadosEpisodios> episodiosTemporada = temporadas.get(i).episodios();
+//            for (int j = 0; j < episodiosTemporada.size(); j++) {
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e-> System.out.println(e.titulo())));
     }
 }
