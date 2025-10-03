a14e19016a6ab8694ae1fc6bdeada4855457e358
Ruan Silva <ruan.silva2002@gmail.com>
2025-10-02 23:17:11 -0300

    Update

--- src/main/java/br/com/ruan/cinevalient/CinevalientApplication.java
+++ src/main/java/br/com/ruan/cinevalient/CinevalientApplication.java
@@ -1,8 +1,6 @@
 package br.com.ruan.cinevalient;
 
-import br.com.ruan.cinevalient.model.DadosSerie;
-import br.com.ruan.cinevalient.service.ConsumoAPI;
-import br.com.ruan.cinevalient.service.ConverteDados;
+import br.com.ruan.cinevalient.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,14 +14,8 @@ public class CinevalientApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		ConsumoAPI consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=Lucifer&apikey=7a9c54d5");
-		System.out.println(json);
+        Principal principal = new Principal();
+        principal.exibeMenu();
 
-		//json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
-		//System.out.println(json);
-        ConverteDados conversor = new ConverteDados();
-        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-        System.out.println(dados);
 	}
 }
