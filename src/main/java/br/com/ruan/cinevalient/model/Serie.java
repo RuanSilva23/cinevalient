c128bf139baf8da97218e88889b14207f84e2935
Ruan Silva <ruan.silva2002@gmail.com>
2025-11-03 22:00:31 -0300

    Update Serie.java

--- src/main/java/br/com/ruan/cinevalient/model/Serie.java
+++ src/main/java/br/com/ruan/cinevalient/model/Serie.java
@@ -24,8 +24,10 @@ public class Serie {
     private String sinopse;
     private String atores;
 
-    @Transient
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
+
+    public Serie() {}
 
     public Serie(DadosSerie dadosSerie) {
         this.titulo = dadosSerie.titulo();
@@ -50,6 +52,7 @@ public class Serie {
     }
 
     public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
         this.episodios = episodios;
     }
 
@@ -119,7 +122,7 @@ public class Serie {
                 "\ntemporadas: " + totalTemporadas +
                 "\natores: " + atores +
                 "\nsinopse: " + sinopse +
-                "\n"
+                "\nepisodios: " + episodios
                 ;
     }
 }
