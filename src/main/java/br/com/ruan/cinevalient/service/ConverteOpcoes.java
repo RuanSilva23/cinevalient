bc035a138d5c126c79b1c5ae0a0ab8dd794a9702
Ruan Silva <ruan.silva2002@gmail.com>
2026-01-04 23:08:38 -0300

    Update ConverteOpcoes.java

--- src/main/java/br/com/ruan/cinevalient/service/ConverteOpcoes.java
+++ src/main/java/br/com/ruan/cinevalient/service/ConverteOpcoes.java
@@ -7,6 +7,11 @@ public enum ConverteOpcoes {
     BUSCAR_SERIE_POR_TITULO,
     BUSCAR_SERIE_POR_ATOR,
     TOP_5_SERIES,
+    BUSCAR_SERIE_CATEGORIA,
+    BUSCAR_SERIE_POR_TEMPORADA_E_AVALIACAO,
+    BUSCAR_SERIE_TRECHO,
+    TOP_EPISODIO_SERIE,
+    BUSCA_EPISODIO_DATA_LANCAMENTO,
     SAIR;
 
     public static ConverteOpcoes fromCodigo(int codigo) {
@@ -17,6 +22,11 @@ public enum ConverteOpcoes {
             case 4 -> BUSCAR_SERIE_POR_TITULO;
             case 5 -> BUSCAR_SERIE_POR_ATOR;
             case 6 -> TOP_5_SERIES;
+            case 7 -> BUSCAR_SERIE_CATEGORIA;
+            case 8 -> BUSCAR_SERIE_POR_TEMPORADA_E_AVALIACAO;
+            case 9 -> BUSCAR_SERIE_TRECHO;
+            case 10 -> TOP_EPISODIO_SERIE;
+            case 11 -> BUSCA_EPISODIO_DATA_LANCAMENTO;
             case 0 -> SAIR;
             default -> throw new IllegalArgumentException("Opção inválida: " + codigo);
         };
