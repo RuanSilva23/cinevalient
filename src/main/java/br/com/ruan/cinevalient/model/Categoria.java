d9e0fcfda3c1ddb6b73924a6286b4b4146d76609
Ruan Silva <ruan.silva2002@gmail.com>
2026-01-04 23:08:54 -0300

    Update Categoria.java

--- src/main/java/br/com/ruan/cinevalient/model/Categoria.java
+++ src/main/java/br/com/ruan/cinevalient/model/Categoria.java
@@ -1,22 +1,34 @@
 package br.com.ruan.cinevalient.model;
 
 public enum Categoria {
-    ACAO("Action"),
-    AVENTURA("Adventure"),
-    DRAMA("Drama"),
-    COMEDIA("Comedy"),
-    FICCAO("Sci-Fi"),
-    CRIME("Crime");
+    ACAO("Action", "Ação"),
+    AVENTURA("Adventure", "Aventura"),
+    DRAMA("Drama", "Drama"),
+    COMEDIA("Comedy", "Comedia"),
+    FICCAO("Sci-Fi", "Ficção"),
+    CRIME("Crime", "Crime"),
+    ROMANCE("Romance", "Romance");
 
     private String categoriaOMDB;
+    private String categoriaEmPortugues;
 
-    Categoria(String categoriaOMDB) {
+    Categoria(String categoriaOMDB, String categoriaEmPortugues) {
         this.categoriaOMDB = categoriaOMDB;
+        this.categoriaEmPortugues = categoriaEmPortugues;
     }
 
     public static Categoria fromString(String text) {
         for (Categoria categoria : Categoria.values()) {
             if (categoria.categoriaOMDB.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+
+    public static Categoria fromPortugues(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaEmPortugues.equalsIgnoreCase(text)) {
                 return categoria;
             }
         }
