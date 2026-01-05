4dca1d3ee664a61f7ecdcee4fdd845444b10991b
Ruan Silva <ruan.silva2002@gmail.com>
2026-01-04 23:08:46 -0300

    Update SerieRepository.java

--- src/main/java/br/com/ruan/cinevalient/repository/SerieRepository.java
+++ src/main/java/br/com/ruan/cinevalient/repository/SerieRepository.java
@@ -1,7 +1,10 @@
 package br.com.ruan.cinevalient.repository;
 
+import br.com.ruan.cinevalient.model.Categoria;
+import br.com.ruan.cinevalient.model.Episodio;
 import br.com.ruan.cinevalient.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +15,20 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
     List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, Double avaliacaoMinima);
 
     List<Serie> findTop5ByOrderByAvaliacaoDesc();
+
+    List<Serie> findByGenero(Categoria categoria);
+
+    List<Serie> findBytotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(Integer Temporadas, Double avaliacao);
+
+    @Query("SELECT s FROM Serie s WHERE s.totalTemporadas <= :temporadas AND s.avaliacao >= :avaliacao")
+    List<Serie> seriesPorTemporadaEAvaliacao(Integer temporadas, Double avaliacao);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE e.titulo ILIKE %:trechoEpisodio%")
+    List<Episodio> episodiosPorTrecho(String trechoEpisodio);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s = :serie ORDER BY e.avaliacao DESC LIMIT 5")
+    List<Episodio> topEpisodiosPorSerie(Serie serie);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s = :serie AND YEAR(e.dataLancamento) >= :dataLancamento")
+    List<Episodio> episodiosPorData(Serie serie, int dataLancamento);
 }
