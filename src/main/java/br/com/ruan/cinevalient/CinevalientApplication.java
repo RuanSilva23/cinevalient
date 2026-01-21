4cdee1bc9bc92a228777d0eb799b28807bdee287
Ruan Silva <ruan.silva2002@gmail.com>
2026-01-21 18:45:38 -0300

    Remove CommandLineRunner implementation from main class
    Refactored CinevalientApplication to no longer implement CommandLineRunner. Initialization logic and dependencies related to Principal and SerieRepository have been removed for a cleaner application entry point.

--- src/main/java/br/com/ruan/cinevalient/CinevalientApplication.java
+++ src/main/java/br/com/ruan/cinevalient/CinevalientApplication.java
@@ -1,24 +1,11 @@
 package br.com.ruan.cinevalient;
 
-import br.com.ruan.cinevalient.principal.Principal;
-import br.com.ruan.cinevalient.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CinevalientApplication implements CommandLineRunner {
-    @Autowired
-    private SerieRepository repository;
+public class CinevalientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CinevalientApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-        Principal principal = new Principal(repository);
-        principal.exibeMenu();
-
 	}
 }
