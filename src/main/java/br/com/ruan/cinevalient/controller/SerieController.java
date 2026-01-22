2a914b6bf67120b4d2ade5c6de836c3355e84bb3
Ruan Silva <ruan.silva2002@gmail.com>
2026-01-22 00:56:20 -0300

    Update SerieController.java

--- src/main/java/br/com/ruan/cinevalient/controller/SerieController.java
+++ src/main/java/br/com/ruan/cinevalient/controller/SerieController.java
@@ -1,12 +1,24 @@
 package br.com.ruan.cinevalient.controller;
 
+import br.com.ruan.cinevalient.dto.SerieDTO;
+import br.com.ruan.cinevalient.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 public class SerieController {
+    @Autowired
+    private SerieRepository serieRepository;
+
     @GetMapping("/series")
-    public String obterSeries() {
-        return "As series vão ser listadas aqui";
+    public List<SerieDTO> obterSeries() {
+        return serieRepository.findAll().stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getAno(), s.getGenero(), s.getTotalTemporadas(), s.getAvaliacao(), s.getSinopse(), s.getAtores(), s.getPoster()))
+                .collect(Collectors.toList());
     }
+
 }
