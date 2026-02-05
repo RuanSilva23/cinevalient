210640ba2451126e4889c14706b8c1bff0d4b232
Ruan Silva <ruan.silva2002@gmail.com>
2026-02-05 20:48:20 -0300

    Update SerieController.java

--- src/main/java/br/com/ruan/cinevalient/controller/SerieController.java
+++ src/main/java/br/com/ruan/cinevalient/controller/SerieController.java
@@ -1,24 +1,34 @@
 package br.com.ruan.cinevalient.controller;
 
 import br.com.ruan.cinevalient.dto.SerieDTO;
-import br.com.ruan.cinevalient.repository.SerieRepository;
+import br.com.ruan.cinevalient.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
-    @Autowired
-    private SerieRepository serieRepository;
 
-    @GetMapping("/series")
+    @Autowired
+    private SerieService service;
+
+    @GetMapping()
     public List<SerieDTO> obterSeries() {
-        return serieRepository.findAll().stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getAno(), s.getGenero(), s.getTotalTemporadas(), s.getAvaliacao(), s.getSinopse(), s.getAtores(), s.getPoster()))
-                .collect(Collectors.toList());
+        return service.obterSerie();
+    }
+
+    @GetMapping("/top5")
+    public List<SerieDTO> obterTop5Series(){
+        return service.obterTop5Series();
+    }
+
+    @GetMapping("/lancamentos")
+    public  List<SerieDTO> obterLancamento(){
+        return service.obterLancamento();
     }
 
 }
